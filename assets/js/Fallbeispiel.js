a72bba8e6e2eb8b7fbdf014796228c0167ff010c
elmar <elmar_pohl@hotmail.com>
2026-04-11 22:09:37 +0200

    Merged branch fixSubmitBtnSamsung,11th attempt

--- assets/js/Fallbeispiel.js
+++ assets/js/Fallbeispiel.js
@@ -9,47 +9,6 @@
  * - airportSearch.js
  */
 
-<<<<<<< HEAD
-/**
- * Handler for the submit event of form #form_search_options.
- * It is triggered by the submit button, or by hitting Enter in a field.
- * 
- * Handling submit is more reliable than handling click, touch, or
- * pointer events on the submit button as different browsers handle such
- * events in different ways. Particularly in the Samsung browser on
- * mobile phones, touching the submit button hardly ever triggers a
- * click event.
- * @param {event} ev - A form's submit event
- */
-function handleFormSearchOptionsSubmit (ev)
-{
-    // Suppress default handling for the submit event.
-    // Default handling would clear the form and the search results
-    // shortly after submit. This makes it virtually impossible to view
-    // the results.
-    // Also we want the search criteria to remain so the user can modify
-    // them without having to re-enter all of them.
-    //
-    // Default handling is also not needed as we don't really want to
-    // submit data to a server. We only want to call the search function.
-    ev.preventDefault();
-
-    let btn = ev.submitter;
-    if (btn == null || btn.id != "btn_search")
-        return;
-    
-    showMatchingRoutes();
-}
-
-
-function installSubmitHandler()
-{
-    let formSearchOptions = document.getElementById ("form_search_options");
-    if (formSearchOptions == null)
-        return;
-
-    formSearchOptions.addEventListener ("submit", handleFormSearchOptionsSubmit);
-=======
 var ignoreBtnSearchEvents = false;
 
 function handleTimeout()
@@ -95,7 +54,6 @@ function installSearchBtnHandlers()
 
     btnSearch.addEventListener ("click", handleBtnSearchClick);
     btnSearch.addEventListener ("pointerdown", handleBtnSearchPtr);
->>>>>>> fixSubmitBtnSamsung
 }
 
 
@@ -103,11 +61,7 @@ function installEventHandlers()
 {
     installAirportSearchEventHandlers();
     installRoutesSearchEventHandlers();
-<<<<<<< HEAD
-    installSubmitHandler();
-=======
     installSearchBtnHandlers();
->>>>>>> fixSubmitBtnSamsung
 }
 
 
